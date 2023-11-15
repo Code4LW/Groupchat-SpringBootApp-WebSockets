0883b2633750bbc0edf917aae8d027cfe4f5587c
Zanggar <zangar992103@gmail.com>
2023-11-15 10:59:18 +0600

    done

---
+++ src/main/java/com/example/groupchat/config/WebSocketEventListener.java
@@ -0,0 +1,34 @@
+package com.example.groupchat.config;
+
+import com.example.groupchat.model.ChatMessage;
+import com.example.groupchat.model.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageSendingOperations;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor =StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .messageType(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageSendingOperations.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+
+}
